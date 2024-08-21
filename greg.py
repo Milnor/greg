16bab2212430d0ed59f5d04fcc8e516ea25b5329
Seaver <Milnor@users.noreply.github.com>
2024-08-20 23:16:53 -0800

    heavy duty linting; move prproject to correct folder

--- greg.py
+++ greg.py
@@ -1,3 +1,5 @@
+"""Stuff invisible keywords into a resume PDF."""
+
 import argparse
 import sys
 from pathlib import Path
@@ -6,23 +8,29 @@ import pymupdf
 
 
 def string_from_file(filename):
-    ''' Return text contents of file as a string '''
-    
+    """Return text contents of file as a string."""
     match Path(filename).suffix:
         case ".pdf":
             # TODO: parse this with pymypdf
-            raise NotImplementedError("PDF is correct type, but not implemented yet")
+            raise NotImplementedError(
+                "PDF is correct type, but not implemented yet"
+            )
         case ".txt":
             with open(filename, "r") as data:
                 return data.read()
         case _:
-            raise TypeError(f"Expected PDF or TXT, not {Path(filename).suffix}")
+            raise TypeError(
+                f"Expected PDF or TXT, not {Path(filename).suffix}"
+            )
+
 
 def validated_arguments(resume, hidden, output, force, verbose):
-    ''' Return valid arguments for main() or raise error '''
-
+    """Return valid arguments for main() or raise error."""
     if verbose:
-        print(f"[!] validated_arguments(resume={resume}, hidden={hidden}, output={output}, force={force}, verbose={verbose})")
+        print(
+            f"[!] validated_arguments(resume={resume}, hidden={hidden}, "
+            "output={output}, force={force}, verbose={verbose})"
+        )
 
     resume = Path(resume)
     hidden = Path(hidden)
@@ -38,9 +46,12 @@ def validated_arguments(resume, hidden, output, force, verbose):
 
     # error if output name already exists if not used with force
     if Path.exists(output) and not force:
-        raise FileExistsError(f"{output} exists. Use -f or --force to overwrite")
+        raise FileExistsError(
+            f"{output} exists. Use -f or --force to overwrite"
+        )
 
-    # output must be a valid pathname to a PDF or autogenerate one based on input filename
+    # output must be a valid pathname to a PDF or autogenerate one
+    # based on input filename
     if output == "autogenerate":
         for x in range(1, 10000):
             possible_path = f"{resume}_{x}.pdf"
@@ -48,27 +59,53 @@ def validated_arguments(resume, hidden, output, force, verbose):
                 output = possible_path
                 break
     if output == "autogenerate":
-        raise FileExistsError(f"Sorry. I couldn't devise an unused output path {resume}_X.pdf")
+        raise FileExistsError(
+            f"Sorry. I couldn't devise an unused output path {resume}_X.pdf"
+        )
 
     return resume, hidden, output, verbose
 
-def main(args):
-    ''' Insert hidden text into a PDF and save it under a new name '''
 
-    parser = argparse.ArgumentParser(prog='greg', description='Insert hidden text into a PDF')
-    parser.add_argument('resume', type=str, help='Original resume as PDF')
-    parser.add_argument('hidden', type=str, help='PDF file, TXT file, or quote delimited string containing text to insert')
-    parser.add_argument('output', type=str, help='Name of output file, must be PDF', default='autogenerate')
-    parser.add_argument('-f', '--force', action='store_true', help='Overwrite output file if it already exists')
-    parser.add_argument('-v', '--verbose', action='store_true', help='Verbose mode for testing and debugging')
+def main(args):
+    """Insert hidden text into a PDF and save it under a new name."""
+    parser = argparse.ArgumentParser(
+        prog="greg", description="Insert hidden text into a PDF"
+    )
+    parser.add_argument("resume", type=str, help="Original resume as PDF")
+    parser.add_argument(
+        "hidden",
+        type=str,
+        help="PDF file, TXT file, or string containing text to insert",
+    )
+    parser.add_argument(
+        "output",
+        type=str,
+        help="Name of output file, must be PDF",
+        default="autogenerate",
+    )
+    parser.add_argument(
+        "-f",
+        "--force",
+        action="store_true",
+        help="Overwrite output file if it already exists",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Verbose mode for testing and debugging",
+    )
     args = parser.parse_args()
-    original, hidden_text, output, verbose = validated_arguments(args.resume, args.hidden, args.output, args.force, args.verbose)
+    original, hidden_text, output, verbose = validated_arguments(
+        args.resume, args.hidden, args.output, args.force, args.verbose
+    )
 
     # Workflow in this block adapted from ChatGPT response
     with pymupdf.open(original) as doc:
-        page = doc[0] # Insert into first page by default
-        page.insert_text((72, 72), hidden_text, fontsize=12, color=(1, 1, 1)) 
+        page = doc[0]  # Insert into first page by default
+        page.insert_text((72, 72), hidden_text, fontsize=12, color=(1, 1, 1))
         doc.save(output)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- test/test_greg.py
+++ test/test_greg.py
@@ -1,29 +1,40 @@
+"""Unit tests and validation of PDF output."""
+
 import unittest
 
 from greg import validated_arguments, string_from_file, main
 
+
 class TestValidation(unittest.TestCase):
+    """Unit tests to validate individual functions."""
 
     def test_main(self):
-        ''' Validate handling of command line arguments '''
+        """Validate handling of command line arguments."""
         self.assertRaises(SystemExit, main, None)
 
     def test_string_from_file(self):
+        """Validate text of file is returned."""
         self.assertRaises(TypeError, string_from_file, "job_description.docx")
-        self.assertRaises(FileNotFoundError, string_from_file, "no_such_job_description.txt")
+        self.assertRaises(
+            FileNotFoundError, string_from_file, "no_such_job_description.txt"
+        )
 
     def test_validated_arguments(self):
+        """Validate handling of arguments."""
         self.assertRaises(TypeError, validated_arguments, None)
 
+
 class TestResults(unittest.TestCase):
+    """Verify resulting PDF met requirements."""
 
     def test_hidden_keywords(self):
-        ''' Verify hidden text exists within output PDF '''
+        """Verify hidden text exists within output PDF."""
         pass
 
     def test_clean_output(self):
-        ''' Verify modified PDF is visually equivalent to input PDF '''
+        """Verify modified PDF is visually equivalent to input PDF."""
         pass
+
 
 if __name__ == "__main__":
     unittest.main()
